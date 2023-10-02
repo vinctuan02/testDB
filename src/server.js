f96b52dabd985ce5bca0d23afd24acd0552a4e03
vinctuan02 <vinctuan02@gmail.com>
2023-10-02 23:52:19 +0700

    oke

---
+++ src/server.js
@@ -0,0 +1,47 @@
+
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize('congngheWeb', 'root', null, {
+    host: 'localhost',
+    port: 3001,
+    dialect: 'mysql',
+    logging: false
+})
+
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
+
+const SinhVien = sequelize.define("sinhvien", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    fistname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+// sequelize.sync().then(() => {
+//     console.log('Book table created successfully!');
+// }).catch((error) => {
+//     console.error('Unable to create table : ', error);
+// });
+
+const svTuan = SinhVien.create({ fistname: 'Tuan1', lastName: "Nguyen" })
+
+// Lưu bản ghi trong luồng
+// sinhvien.save({ stream: true }, (err, sinhvien) => {
+//     // ...
+// });
+
+console.log("success")
